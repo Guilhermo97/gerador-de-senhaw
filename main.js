61e92457a89e3c7285504a5077d76c7e6f71fee6
Guilhermo97 <146236423+Guilhermo97@users.noreply.github.com>
2025-06-09 08:28:03 -0300

    .

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 document.querySelector('.parametro-senha_botao');
 const numeroSenha = document.querySelector('.parametro-senha_texto');
 numeroSenha.textContent = 5;
+let tamanhoSenha = 12;
+
+numeroSenha.textContent = tamanhoSenha;
 
 const botoes = document.querySelector('.parametro-senha_botao');
+botoes[0].onclick = diminuirTamanho;
 
 console.log(botoes);
+
+function diminuirTamanho(){
+    tamanhoSenha = tamanhoSenha -1;
+    numeroSenha.textContent = tamanhoSenha
+}
